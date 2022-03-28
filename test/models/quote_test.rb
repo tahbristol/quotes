c195764997db218a681970ba70c97ce1fa447a6d
tahbristol <tahbristol@gmail.com>
2022-03-27 21:43:25 -0400

    Stopped with turbo from _top

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,5 @@
 require "test_helper"
 
 class QuoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
 end
--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -5,21 +5,6 @@ class QuotesTest < ApplicationSystemTestCase
     @quote = quotes(:first)
   end
 
-  test "Creating a new quote" do 
-
-    visit quotes_path
-    assert_selector "h1", text: "Quotes"
-
-    click_on "New quote"
-    assert_selector "h1", text: "New quote"
-
-    fill_in "Name", with: "Capybara quote"
-    click_on "Create quote"
-
-    assert_selector "h1", text: "Quotes"
-    assert_text "Capybara quote"
-  end
-
   test "Showing a quote" do
     visit quotes_path
     click_link @quote.name
@@ -27,14 +12,28 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: @quote.name
   end
 
+  test "Creating a new quote" do
+    visit quotes_path
+    assert_selector "h1", text: "Quotes"
+
+    click_on "New quote"
+    fill_in "Name", with: "Capybara quote"
+
+    assert_selector "h1", text: "Quotes"
+    click_on "Create quote"
+
+    assert_selector "h1", text: "Quotes"
+    assert_text "Capybara quote"
+  end
+
   test "Updating a quote" do
     visit quotes_path
     assert_selector "h1", text: "Quotes"
 
     click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit quote"
-
     fill_in "Name", with: "Updated quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Update quote"
 
     assert_selector "h1", text: "Quotes"
